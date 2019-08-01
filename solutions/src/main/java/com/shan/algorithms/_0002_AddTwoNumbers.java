a11498af6d0ccf91e2de0f56881c9c3fdcdc0fb1
shanmengm <mengm.shan@gmail.com>
2019-08-01 17:49:49 +0800

    add two numbers

---
+++ solutions/src/main/java/com/shan/algorithms/_0002_AddTwoNumbers.java
@@ -0,0 +1,57 @@
+package com.shan.algorithms;
+
+
+/**
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * 示例：
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ */
+public class _0002_AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode listNode = new ListNode(0);
+        ListNode curr = listNode;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
+            if (sum < 10) {
+                curr.next = new ListNode(sum);
+                carry = 0;
+            } else {
+                curr.next = new ListNode(sum - 10);
+                carry = 1;
+            }
+
+            curr = curr.next;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+
+        if (carry == 1) {
+            curr.next = new ListNode(1);
+        }
+        return listNode.next;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
